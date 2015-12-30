d4d4279697409b243baaff7433de7afc0c67e4ba
jjhassan <jjohassan@gmail.com>
2015-12-30 19:12:19 +0000

    First draft mission prototype

--- lib/mission.h
+++ lib/mission.h
@@ -1,11 +1,22 @@
 #pragma once
-
+#include <lib/player.h>
 class Game;
-
+class Player;
 class IMission
 {
 public:
-	virtual bool Check(Game& aGame) = 0;
+	virtual bool Check(Game& aGame, Player& aEvaluatedPlayer) = 0;
 	virtual ~IMission() {}
 };
 
+
+
+class Mission : public IMission
+{
+public:
+	Mission();
+	~Mission();
+public: //IMission
+	bool Check(Game& aGame, Player& aEvaluatedPlayer) override;
+
+};
